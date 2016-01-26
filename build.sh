4b5e00def8d82914debbff5fe1491b89b647c860
CoolAs <goldenscruff@gmail.com>
2016-01-27 08:58:28 +1100

    Added basic tinsel -> C++14 compiler

---
+++ build.sh
@@ -0,0 +1,36 @@
+types=("u8" "s8" "u16" "s16" "u32" "s32")
+
+echo "----Building tnl2c libraries----"
+
+cd libs/C
+
+for i in "${types[@]}"
+do
+echo " - $i""_array.h"
+sed -e "s/_type/$i/g" < templates/array_template > h/"$i"_array.h
+done
+
+echo " - tnl2c.h"
+cat templates/tnl2c_template > h/tnl2c.h
+for i in "${types[@]}"
+do
+echo "#include \"$i""_array.h\"" >> h/tnl2c.h
+done
+echo "#include \"String.h\"" >> h/tnl2c.h
+cd ../../
+
+echo "----Building the stdlib for C---"
+
+cstdlibfiles=("String" "IO" "FileIO" "ConsoleIO")
+for i in "${cstdlibfiles[@]}"
+do
+	if [ -e "libs/C/$i.tnl" ]
+	then
+		echo " - $i"
+		bash -e ctinsel.sh "libs/C/$i.tnl" "libs/C/h/$i.h"
+	elif [ -e "libs/base/$i.tnl" ]
+	then
+		echo " - $i (base)"
+		bash -e ctinsel.sh "libs/base/$i.tnl" "libs/C/h/$i.h"
+	fi
+done
---
+++ ctinsel.sh
@@ -0,0 +1,64 @@
+filename=$(basename "$1")
+mainfunction=""
+if [ ! -z "$3" ]
+then
+ # This is where we need to do function parsing to create help and main() functions for the entry point
+ cmdargs=$( sed --posix -n -e "s/int $3.*(\(.*\))/\1/p" $1 )
+ cmdargsarr=$(echo $cmdargs | sed -e "s/\s*,\s*/\n/g" | sed -e "s/ /__ctinsel__/g")
+ mainfunction="int $3($cmdargs); int main(int argc,char** argv){"
+ for x in $cmdargsarr
+ do
+
+  name=$(echo $x | sed --posix -e "s/.*__ctinsel__\(.*\)/\1/")
+  type=$(echo $x | sed --posix -e "s/\(.*\)__ctinsel__.*/\1/")
+  mainfunction="$mainfunction $type $name"
+  if [ "$type" = "bool" ]
+  then
+   mainfunction="$mainfunction = false;"
+	elif [ "$type" = "int" ]
+	then
+		mainfunction="$mainfunction = 0;"
+  else
+   mainfunction="$mainfunction;"
+  fi
+ 
+ done
+ mainfunction="$mainfunction for (int i = 2; i < argc; i+=2){ "
+
+
+ for x in $cmdargsarr
+ do
+
+  name=$(echo $x | sed --posix -e "s/.*__ctinsel__\(.*\)/\1/")
+  type=$(echo $x | sed --posix -e "s/\(.*\)__ctinsel__.*/\1/")
+  mainfunction="$mainfunction if(strcmp(\"--$name\",argv[i-1])==0) "
+  if [ "$type" = "bool" ]
+  then
+   mainfunction="$mainfunction $name=strcmp(argv[i],\"true\")==0;"
+  elif [ "$type" = "String" ]
+  then
+   mainfunction="$mainfunction $name=String(argv[i]);"
+  elif [ "$type" = "int" ]
+  then
+   mainfunction="$mainfunction $name=std::stoi(argv[i]);"
+  fi
+ done
+ mainfunction="$mainfunction } return $3("
+
+ comma=false
+ for x in $cmdargsarr
+ do
+  name=$(echo $x | sed --posix -e "s/.*__ctinsel__\(.*\)/\1/")
+  if $comma
+  then
+   mainfunction="$mainfunction ,"
+  fi
+  mainfunction="$mainfunction $name"
+  comma=true
+ done 
+ mainfunction="$mainfunction ); }" 
+fi
+
+echo "#pragma once
+#include \"tnl2c.h\" 
+$mainfunction" | cat - $1 | sed --posix -e "s/\([a-z]*\) array/\1_array/g" -e "s/import \(.*\)/#include \\\"\1.h\\\"/g" -e "s/extends/\: public/g" -e "s/self\./this\-\>/g" -e "s/self/this/g" > $2
