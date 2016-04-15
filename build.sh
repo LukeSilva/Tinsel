7474c480459f2745ba01890f62f95f8618d4f352
CoolAs <goldenscruff@gmail.com>
2016-04-15 11:18:04 +1000

    C++14 enviroment supports the array of any type

--- build.sh
+++ build.sh
@@ -1,31 +1,20 @@
-types=("u8" "s8" "u16" "s16" "u32" "s32" "int" "bool")
 
 echo "----Building tnl2c libraries----"
 
 cd libs/C
 mkdir -p h
-for i in "${types[@]}"
-do
-echo " - $i""_array.h"
-sed -e "s/_type/$i/g" < templates/array_template > h/"$i"_array.h
-done
+
+cp tinsel_array.h h/
 
 echo " - tnl2c.h"
 cat templates/tnl2c_template > h/tnl2c.h
-for i in "${types[@]}"
-do
-echo "#include \"$i""_array.h\"" >> h/tnl2c.h
-done
+
+
+
 echo "#include \"String.h\"" >> h/tnl2c.h
 echo "#include \"ConsoleIO.h\"
 Console std_in;
 Console std_out;" >> h/tnl2c.h
-
-
-echo "----Building the String array type----"
-sed -e "s/_type/String/g" < templates/array_template > h/String_array.h
-echo "#include \"String_array.h\"" >> h/tnl2c.h
-echo " - String_array.h"
 
 cd ../../
 
